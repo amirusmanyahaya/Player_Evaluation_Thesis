f644cb1f7f9d4ee0a8983f4a3dc90ff8ea914a25
Yahaya Usman <yahus591@student.liu.se>
2021-06-26 04:48:01 +0200

    added the extract_season function

--- database.py
+++ database.py
@@ -16,15 +16,19 @@ def create_connection(user: str, password: str, host: str, database: str):
     Returns: connection
 
     """
-    engine = create_engine(f"mysql+mysqlconnector://{user}:{password}@{host}/{database}")
+    engine = create_engine(
+        f"mysql+mysqlconnector://{user}:{password}@{host}/{database}", echo=False
+    )
     return engine
 
 
 if __name__ == "__main__":
-    engine = create_connection(user=os.environ["db_user"],
-                               password=os.environ["db_pass"],
-                               host=os.environ["db_host"],
-                               database=os.environ["db_name"])
+    engine = create_connection(
+        user=os.environ["db_user"],
+        password=os.environ["db_pass"],
+        host=os.environ["db_host"],
+        database=os.environ["db_name"],
+    )
     with engine.begin() as connection:
         query = text("describe play_by_play_events")
         result = connection.execute(query).fetchall()
--- populate_field.py
+++ populate_field.py
@@ -5,14 +5,13 @@ from sqlalchemy import ForeignKey
 from sqlalchemy import Integer
 from sqlalchemy import Text
 from sqlalchemy import Time
+from sqlalchemy import text
+from sqlalchemy import Table
+from sqlalchemy import MetaData
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from tqdm import tqdm
 
-engine = database.create_connection(user=os.environ["db_user"],
-                                    password=os.environ["db_pass"],
-                                    host=os.environ["db_host"],
-                                    database=os.environ["db_name"])
-session = sessionmaker(bind=engine)()
 Base = declarative_base()
 
 
@@ -80,20 +79,113 @@ class PlayByPlayEvents(Base):
     HomePlayer9 = Column(Integer, ForeignKey(Player.PlayerId))
 
 
+class ExtractedPlayByPlay(Base):
+    __tablename__ = "extracted_play_by_play"
+    GameId = Column(Integer, ForeignKey(Game.GameId), primary_key=True)
+    AwayTeamId = Column(Integer, ForeignKey(Team.TeamId))
+    HomeTeamId = Column(Integer, ForeignKey(Team.TeamId))
+    ActionSequence = Column(Integer)
+    EventNumber = Column(Integer)
+    PeriodNumber = Column(Integer)
+    EventTime = Column(Time)
+    EventType = Column(Text)
+    ExternalEventId = Column(Integer)
+    AwayPlayer1 = Column(Integer, ForeignKey(Player.PlayerId))
+    AwayPlayer2 = Column(Integer, ForeignKey(Player.PlayerId))
+    AwayPlayer3 = Column(Integer, ForeignKey(Player.PlayerId))
+    AwayPlayer4 = Column(Integer, ForeignKey(Player.PlayerId))
+    AwayPlayer5 = Column(Integer, ForeignKey(Player.PlayerId))
+    AwayPlayer6 = Column(Integer, ForeignKey(Player.PlayerId))
+    AwayPlayer7 = Column(Integer, ForeignKey(Player.PlayerId))
+    AwayPlayer8 = Column(Integer, ForeignKey(Player.PlayerId))
+    AwayPlayer9 = Column(Integer, ForeignKey(Player.PlayerId))
+    HomePlayer1 = Column(Integer, ForeignKey(Player.PlayerId))
+    HomePlayer2 = Column(Integer, ForeignKey(Player.PlayerId))
+    HomePlayer3 = Column(Integer, ForeignKey(Player.PlayerId))
+    HomePlayer4 = Column(Integer, ForeignKey(Player.PlayerId))
+    HomePlayer5 = Column(Integer, ForeignKey(Player.PlayerId))
+    HomePlayer6 = Column(Integer, ForeignKey(Player.PlayerId))
+    HomePlayer7 = Column(Integer, ForeignKey(Player.PlayerId))
+    HomePlayer8 = Column(Integer, ForeignKey(Player.PlayerId))
+    HomePlayer9 = Column(Integer, ForeignKey(Player.PlayerId))
+
+
+# returns the
+def extract_season(engine, season, season_type="All", session=sessionmaker):
+    # adding the season to a new table new season
+    text_query = text("DROP TABLE IF EXISTS extracted_play_by_play;")
+    connection = engine.connect()
+    connection.execute(text_query)
+    print("I dropped extracted_play_by_play")
+    Base.metadata.create_all(engine)
+
+    # extracting the specific season
+    print("I created the table extracted_play_by_play")
+    result = None
+    session1 = session(bind=engine)()
+    if season_type == "Regular Season":
+        result = (
+            session1.query(PlayByPlayEvents)
+            .join(Game, PlayByPlayEvents.GameId == Game.GameId)
+            .filter(Game.Season == season, Game.SeasonType == "Regular Season")
+            .all()
+        )
+    elif season_type == "Playoffs":
+        result = (
+            session1.query(PlayByPlayEvents)
+            .join(Game, PlayByPlayEvents.GameId == Game.GameId)
+            .filter(Game.Season == season, Game.SeasonType == "Playoffs")
+            .all()
+        )
+    else:
+        result = (
+            session1.query(PlayByPlayEvents)
+            .join(Game, PlayByPlayEvents.GameId == Game.GameId)
+            .filter(Game.Season == season)
+            .all()
+        )
+
+    # populates ExtractedEventPlayByPlay table
+    for row in tqdm(result):
+        session1.add(
+            ExtractedPlayByPlay(
+                GameId=row.GameId,
+                AwayTeamId=row.AwayTeamId,
+                HomeTeamId=row.HomeTeamId,
+                ActionSequence=row.ActionSequence,
+                EventNumber=row.EventNumber,
+                PeriodNumber=row.PeriodNumber,
+                EventTime=row.EventTime,
+                ExternalEventId=row.ExternalEventId,
+                AwayPlayer1=row.AwayPlayer1,
+                AwayPlayer2=row.AwayPlayer2,
+                AwayPlayer3=row.AwayPlayer3,
+                AwayPlayer4=row.AwayPlayer4,
+                AwayPlayer5=row.AwayPlayer5,
+                AwayPlayer6=row.AwayPlayer6,
+                AwayPlayer7=row.AwayPlayer7,
+                AwayPlayer8=row.AwayPlayer8,
+                AwayPlayer9=row.AwayPlayer9,
+                HomePlayer1=row.HomePlayer1,
+                HomePlayer2=row.HomePlayer2,
+                HomePlayer3=row.HomePlayer3,
+                HomePlayer4=row.HomePlayer4,
+                HomePlayer5=row.HomePlayer5,
+                HomePlayer6=row.HomePlayer6,
+                HomePlayer7=row.HomePlayer7,
+                HomePlayer8=row.HomePlayer8,
+                HomePlayer9=row.HomePlayer9,
+            )
+        )
+    session1.commit()
+
+
 if __name__ == "__main__":
-    # result = session.query(Game).filter(Game.Season == "2010-2011", Game.SeasonType != "Playoffs").all()
-    # for row in result:
-    #     print(f"{row.GameId}\t{row.Season}\t{row.SeasonType}")
-
-    # result = session.query(Player).filter(Player.PlayerId == 8445176).all()
-    # for row in result:
-    #     print(f"{row.PlayerId}\t{row.PlayerName}")
-
-    # result = session.query(Team).filter(Team.League == "NHL").all()
-    # for row in result:
-    #     print(f"{row.TeamId}\t{row.TeamName}")
-
-    # result = session.query(PlayByPlayEvents).filter(PlayByPlayEvents.AwayPlayer1 == 8445176).all()
-    # for row in result:
-    #     print(f"{row.GameId}\t{row.EventTime}\t{row.EventType}")
-    pass
+    engine = database.create_connection(
+        user=os.environ["db_user"],
+        password=os.environ["db_pass"],
+        host=os.environ["db_host"],
+        database=os.environ["db_name"],
+    )
+    season = "2010-2011"
+    extract_season(engine, season=season, season_type="Regular Season")
